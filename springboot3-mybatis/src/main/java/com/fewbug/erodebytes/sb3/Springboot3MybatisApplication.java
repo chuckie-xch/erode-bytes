caff7a9d03b56835afc8ea8e068a292ec52be700
chunhang.xch <1205241831@qq.com>
2024-07-06 00:09:48 +0800

    Initial commit: Set up the project structure and basic configuration.

---
+++ springboot3-mybatis/src/main/java/com/fewbug/erodebytes/sb3/Springboot3MybatisApplication.java
@@ -0,0 +1,13 @@
+package com.fewbug.erodebytes.sb3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot3MybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot3MybatisApplication.class, args);
+	}
+
+}
---
+++ springboot3-mybatis/src/test/java/com/fewbug/erodebytes/sb3/Springboot3MybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fewbug.erodebytes.sb3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3MybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
